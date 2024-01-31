0dd822b657bdfd69619d57fedfba0ebbee482b8b
Usuario <Usuario@DESKTOP-5MDT9HM>
2024-01-31 08:59:24 -0300

    Agregar archivos de proyecto.

---
+++ Descartables_MF/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Descartables_MF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_open_file = new System.Windows.Forms.Button();
+            this.btn_print_ticket = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_add_product = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btn_clean_fields = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_employee_name = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label_file_name = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_open_file
+            // 
+            this.btn_open_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_open_file.Location = new System.Drawing.Point(307, 62);
+            this.btn_open_file.Name = "btn_open_file";
+            this.btn_open_file.Size = new System.Drawing.Size(111, 30);
+            this.btn_open_file.TabIndex = 2;
+            this.btn_open_file.Text = "Cargar archivo";
+            this.btn_open_file.UseVisualStyleBackColor = true;
+            this.btn_open_file.Click += new System.EventHandler(this.btn_open_file_Click);
+            // 
+            // btn_print_ticket
+            // 
+            this.btn_print_ticket.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_print_ticket.Location = new System.Drawing.Point(457, 615);
+            this.btn_print_ticket.Name = "btn_print_ticket";
+            this.btn_print_ticket.Size = new System.Drawing.Size(149, 43);
+            this.btn_print_ticket.TabIndex = 5;
+            this.btn_print_ticket.Text = "Imprimir comprobante";
+            this.btn_print_ticket.UseVisualStyleBackColor = true;
+            this.btn_print_ticket.Click += new System.EventHandler(this.btn_print_ticket_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 168);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(697, 131);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // btn_add_product
+            // 
+            this.btn_add_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add_product.Location = new System.Drawing.Point(330, 321);
+            this.btn_add_product.Name = "btn_add_product";
+            this.btn_add_product.Size = new System.Drawing.Size(128, 42);
+            this.btn_add_product.TabIndex = 7;
+            this.btn_add_product.Text = "Agregar producto seleccionado";
+            this.btn_add_product.UseVisualStyleBackColor = true;
+            this.btn_add_product.Click += new System.EventHandler(this.btn_add_product_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(52, 386);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(697, 137);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // btn_clean_fields
+            // 
+            this.btn_clean_fields.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clean_fields.Location = new System.Drawing.Point(200, 615);
+            this.btn_clean_fields.Name = "btn_clean_fields";
+            this.btn_clean_fields.Size = new System.Drawing.Size(149, 43);
+            this.btn_clean_fields.TabIndex = 9;
+            this.btn_clean_fields.Text = "Limpiar todos los campos";
+            this.btn_clean_fields.UseVisualStyleBackColor = true;
+            this.btn_clean_fields.Click += new System.EventHandler(this.btn_clean_fields_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 552);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Nombre del empleado:";
+            // 
+            // txt_employee_name
+            // 
+            this.txt_employee_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_employee_name.Location = new System.Drawing.Point(372, 549);
+            this.txt_employee_name.Name = "txt_employee_name";
+            this.txt_employee_name.Size = new System.Drawing.Size(218, 22);
+            this.txt_employee_name.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Descartables_MF.Properties.Resources.LogoEmpresa;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(211, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(304, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Buscar";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(359, 125);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(208, 22);
+            this.txtSearch.TabIndex = 14;
+            // 
+            // label_file_name
+            // 
+            this.label_file_name.AutoSize = true;
+            this.label_file_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_file_name.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label_file_name.Location = new System.Drawing.Point(424, 69);
+            this.label_file_name.MaximumSize = new System.Drawing.Size(322, 0);
+            this.label_file_name.Name = "label_file_name";
+            this.label_file_name.Size = new System.Drawing.Size(0, 16);
+            this.label_file_name.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 700);
+            this.Controls.Add(this.label_file_name);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_employee_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_clean_fields);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btn_add_product);
+            this.Controls.Add(this.btn_open_file);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_print_ticket);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Descartables Mercado Fresco";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_open_file;
+        private System.Windows.Forms.Button btn_print_ticket;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_add_product;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btn_clean_fields;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_employee_name;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label_file_name;
+    }
+}
+
---
+++ Descartables_MF/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing.Printing;
+
+namespace Descartables_MF
+{
+    public partial class Form1 : Form
+    {
+        private DataTable originalDataTable;  // Declaración de la variable
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.CellClick += dataGridView_CellClick;
+            dataGridView2.CellClick += dataGridView_CellClick;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.KeyDown += dataGridView1_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSearch.Enabled = false;
+        }
+
+        private void btn_open_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "Excel Sheet(*.xlsx)|*.xlsx|All Files(*.*)|*.*";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = op.FileName;
+                string con = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+                con = string.Format(con, filepath, "yes");
+                OleDbConnection excelconnection = new OleDbConnection(con);
+                excelconnection.Open();
+                DataTable dtexcel = excelconnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string excelsheet = dtexcel.Rows[0]["TABLE_NAME"].ToString();
+                OleDbCommand com = new OleDbCommand("Select * from [" + excelsheet + "]", excelconnection);
+                OleDbDataAdapter oda = new OleDbDataAdapter(com);
+                DataTable dt = new DataTable();
+                oda.Fill(dt);
+                excelconnection.Close();
+                label_file_name.Text = Path.GetFileName(filepath);
+                originalDataTable = dt;  // Asigna originalDataTable al DataTable cargado
+                dataGridView1.DataSource = dt;
+                txtSearch.Enabled = true;
+
+                // Agrega las columnas de dataGridView1 a dataGridView2
+                dataGridView2.Columns.Clear(); // Limpia las columnas existentes en dataGridView2
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    dataGridView2.Columns.Add((DataGridViewColumn)column.Clone());
+                }
+            }
+        }
+
+        private void btn_add_product_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (dataGridView2.Columns.Count == 0)
+                {
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                    {
+                        dataGridView2.Columns.Add((DataGridViewColumn)col.Clone());
+                    }
+                }
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                DataGridViewRow newRow = (DataGridViewRow)selectedRow.Clone();
+
+                for (int i = 0; i < selectedRow.Cells.Count; i++)
+                {
+                    newRow.Cells[i].Value = selectedRow.Cells[i].Value;
+                }
+                dataGridView2.Rows.Add(newRow);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione una fila en la tabla de arriba", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;  // Indica que la tecla Enter ha sido manejada
+
+                // Verifica que haya al menos una fila seleccionada en dataGridView1
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    // Obtiene la fila seleccionada
+                    DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                    // Clona la fila seleccionada
+                    DataGridViewRow newRow = (DataGridViewRow)selectedRow.Clone();
+
+                    // Copia los valores de la fila seleccionada a la nueva fila
+                    for (int i = 0; i < selectedRow.Cells.Count; i++)
+                    {
+                        newRow.Cells[i].Value = selectedRow.Cells[i].Value;
+                    }
+
+                    // Agrega la nueva fila a dataGridView2
+                    dataGridView2.Rows.Add(newRow);
+                }
+            }
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dataGridView = (DataGridView)sender;
+
+            if (e.RowIndex >= 0)
+            {
+                dataGridView.Rows[e.RowIndex].Selected = true;
+            }
+        }
+
+        private void btn_print_ticket_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay datos en dataGridView2
+            if (dataGridView2.Rows.Count > 0)
+            {
+                PrintDocument pd = new PrintDocument();
+
+                // Asocia el evento de impresión
+                pd.PrintPage += new PrintPageEventHandler(PrintDocument_PrintPage);
+
+                // Configura el tamaño del papel (por ejemplo, carta)
+                pd.DefaultPageSettings.PaperSize = new PaperSize("CustomSize", 314, 390); // Ancho x Alto en cien milésimas de pulgada
+
+                // Muestra el cuadro de diálogo de impresión
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = pd;
+
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pd.Print();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No hay datos para imprimir en el ticket.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // Verifica si hay datos en dataGridView2
+            if (dataGridView2 != null && dataGridView2.Rows.Count > 0)
+            {
+                // Dibuja una imagen en el encabezado
+                Image headerImage = Properties.Resources.LogoEmpresa;
+                e.Graphics.DrawImage(headerImage, new PointF(20, 10));  // Ajusta la posición según tus necesidades
+
+                int y = 200; // Posición vertical inicial después de la imagen
+
+                // Imprime los encabezados de las columnas
+                int xHeader = 20;
+                foreach (DataGridViewColumn column in dataGridView2.Columns)
+                {
+                    e.Graphics.DrawString(column.HeaderText, new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new PointF(xHeader, y - 30));
+                    xHeader += 60; // Ajusta la posición horizontal para la siguiente columna
+                }
+
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    // Verifica si la fila y las celdas no son null
+                    if (row != null && row.Cells.Count > 0)
+                    {
+                        int x = 20; // Posición horizontal inicial
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            // Verifica si el valor de la celda no es null
+                            if (cell.Value != null)
+                            {
+                                e.Graphics.DrawString(cell.Value.ToString(), dataGridView2.Font, Brushes.Black, new PointF(x, y));
+                                x += 60; // Ajusta la posición horizontal para la siguiente celda
+                            }
+                        }
+                        y += 20; // Ajusta la posición vertical para la siguiente fila
+                    }
+                }
+
+                string nombreEmpleado = txt_employee_name.Text;
+                DateTime fecha = DateTime.Now;
+                e.Graphics.DrawString("Empleado: " + nombreEmpleado, new Font("Arial", 10, FontStyle.Regular), Brushes.Black, new PointF(20, y));
+                y += 20;
+                e.Graphics.DrawString(fecha.ToString(), new Font("Arial", 10, FontStyle.Regular), Brushes.Black, new PointF(20, y));
+                y += 60;
+                e.Graphics.DrawString("Firma: ______________________________", new Font("Arial", 10, FontStyle.Regular), Brushes.Black, new PointF(20, y));
+
+                // Configura el tamaño del ticket en píxeles
+                int ticketWidthInPixels = 302;
+                int ticketHeightInPixels = 375;
+
+                // Calcula el factor de escala para ajustar el contenido al tamaño deseado
+                float scaleX = (float)ticketWidthInPixels / e.PageSettings.PrintableArea.Width;
+                float scaleY = (float)ticketHeightInPixels / e.PageSettings.PrintableArea.Height;
+
+                // Aplica la escala al contenido del ticket
+                e.Graphics.ScaleTransform(scaleX, scaleY);
+
+                // Configura el tamaño del papel
+                e.PageSettings.PaperSize = new PaperSize("Custom", 314, 390); // Ancho x Alto en cien milésimas de pulgada
+
+                // Calcula la posición de inicio para centrar el contenido en la página
+                int startX = (e.PageBounds.Width - dataGridView2.Width) / 2;
+                int startY = (e.PageBounds.Height - dataGridView2.Height) / 2;
+
+                // Dibuja el contenido (dataGridView2) en la posición calculada
+                e.Graphics.TranslateTransform(startX, startY);
+                dataGridView2.DrawToBitmap(new Bitmap(dataGridView2.Width, dataGridView2.Height), new Rectangle(0, 0, dataGridView2.Width, dataGridView2.Height));
+                e.Graphics.TranslateTransform(-startX, -startY);
+            }
+            else
+            {
+                MessageBox.Show("No hay datos para imprimir en el ticket.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+
+        private void btn_clean_fields_Click(object sender, EventArgs e)
+        {
+            txt_employee_name.Clear();
+
+            if (dataGridView2.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    dataGridView2.Rows.Clear();
+                }
+
+                RestoreOriginalData();
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string[] searchTerms = txtSearch.Text.ToLower().Split(' ');
+
+            if (searchTerms.Length > 0)
+            {
+                BindingSource bs = new BindingSource();
+                bs.DataSource = originalDataTable;  // originalDataTable es la DataTable original cargada en dataGridView1
+
+                List<string> filters = new List<string>();
+
+                // Construye una lista de fragmentos de búsqueda
+                foreach (string term in searchTerms)
+                {
+                    filters.Add($"Convert({originalDataTable.Columns[1].ColumnName}, 'System.String') LIKE '%{term}%'");
+                }
+
+                // Combina los fragmentos con el operador AND
+                string combinedFilter = string.Join(" AND ", filters);
+
+                // Aplica el filtro
+                bs.Filter = combinedFilter;
+
+                dataGridView1.DataSource = bs;
+            }
+            else
+            {
+                // Si no hay fragmentos de búsqueda, restaura los datos originales
+                RestoreOriginalData();
+            }
+        }
+
+
+
+        private void RestoreOriginalData()
+        {
+            // Restaura los datos originales
+            if (originalDataTable != null)
+            {
+                dataGridView1.DataSource = originalDataTable;
+                txtSearch.Text = "";
+            }
+        }
+    }
+}
